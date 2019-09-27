efcee079abdc8cd925e97db8a78ec61259bff2b0
Cronos <eabz@polispay.org>
2019-09-27 13:01:10 -0500

    Initial tests

---
+++ tokens/ppat/ppat_test.go
@@ -0,0 +1,38 @@
+package ppat
+
+import (
+	"encoding/json"
+	"github.com/grupokindynos/common/jwt"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"testing"
+)
+
+const (
+	TestURL        = "localhost/test-url"
+	TestMasterPass = "1234567890"
+	TestFBToken    = "firebase-jwt"
+)
+
+var (
+	TestPayload = []string{"string1", "string2"}
+)
+
+func TestCreatePPATToken(t *testing.T) {
+	req, err := CreatePPATToken("POST", TestURL, TestFBToken, TestMasterPass, TestPayload)
+	assert.Nil(t, err)
+	token := req.Header.Get("token")
+	url := req.URL
+	assert.Equal(t, TestFBToken, token)
+	assert.Equal(t, TestURL, url.Path)
+	body, _ := ioutil.ReadAll(req.Body)
+	var bodyToken string
+	err = json.Unmarshal(body, &bodyToken)
+	assert.Nil(t, err)
+	decryptedBody, err := jwt.DecryptJWE(TestMasterPass, bodyToken)
+	assert.Nil(t, err)
+	var bodyFormat []string
+	err = json.Unmarshal(decryptedBody, &bodyFormat)
+	assert.Nil(t, err)
+	assert.Equal(t, TestPayload, bodyFormat)
+}
