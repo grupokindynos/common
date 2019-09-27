5b4d435456d614f503de899b4ce6649fb74b2251
Cronos <eabz@polispay.org>
2019-09-27 16:11:57 -0500

    Fix response header

--- responses/responses.go
+++ responses/responses.go
@@ -24,6 +24,7 @@ func GlobalResponseNoAuth(c *gin.Context) *gin.Context {
 
 // GlobalResponseNoAuth is used to wrap all non-auth API responses under the same model.
 func GlobalResponseMRT(header string, body string, c *gin.Context) *gin.Context {
-	c.JSON(200, gin.H{"service": header, "data": body, "status": 1})
+	c.Header("service", header)
+	c.JSON(200, gin.H{"data": body, "status": 1})
 	return c
 }
