12079fa61525bf07af618d05d017a27d0fa18318
Cronos <eabz@polispay.org>
2019-09-30 19:41:43 -0500

    Migrate to firestore

--- hestia/models.go
+++ hestia/models.go
@@ -6,140 +6,140 @@ type Response struct {
 }
 
 type Card struct {
-	Address    string `bson:"address" json:"address"`
-	CardCode   string `bson:"card_code" json:"card_code"`
-	CardNumber string `bson:"card_number" json:"card_number"`
-	City       string `bson:"city" json:"city"`
-	Email      string `bson:"email" json:"email"`
-	FirstName  string `bson:"firstname" json:"firstname"`
-	LastName   string `bson:"lastname" json:"lastname"`
-	UID        string `bson:"uid" json:"uid"`
+	Address    string `firestore:"address" json:"address"`
+	CardCode   string `firestore:"card_code" json:"card_code"`
+	CardNumber string `firestore:"card_number" json:"card_number"`
+	City       string `firestore:"city" json:"city"`
+	Email      string `firestore:"email" json:"email"`
+	FirstName  string `firestore:"firstname" json:"firstname"`
+	LastName   string `firestore:"lastname" json:"lastname"`
+	UID        string `firestore:"uid" json:"uid"`
 }
 
 type Pin struct {
-	CardCode string `bson:"card_code" json:"card_code"`
-	PinCode  string `bson:"pin_code" json:"pin_code"`
+	CardCode string `firestore:"card_code" json:"card_code"`
+	PinCode  string `firestore:"pin_code" json:"pin_code"`
 }
 
 type Coin struct {
-	Ticker            string   `bson:"ticker" json:"ticker"`
-	ShiftAvailable    bool     `bson:"shift_available" json:"shift_available"`
-	DepositAvailable  bool     `bson:"deposit_available" json:"deposit_available"`
-	VouchersAvailable bool     `bson:"vouchers_available" json:"vouchers_available"`
-	OrdersAvailable   bool     `bson:"orders_available" json:"orders_available"`
-	Balances          Balances `bson:"balances" json:"balances"`
+	Ticker            string   `firestore:"ticker" json:"ticker"`
+	ShiftAvailable    bool     `firestore:"shift_available" json:"shift_available"`
+	DepositAvailable  bool     `firestore:"deposit_available" json:"deposit_available"`
+	VouchersAvailable bool     `firestore:"vouchers_available" json:"vouchers_available"`
+	OrdersAvailable   bool     `firestore:"orders_available" json:"orders_available"`
+	Balances          Balances `firestore:"balances" json:"balances"`
 }
 
 type Balances struct {
-	HotWallet float64 `bson:"hot_wallet" json:"hot_wallet"`
-	Exchanges float64 `bson:"exchanges" json:"exchanges"`
+	HotWallet float64 `firestore:"hot_wallet" json:"hot_wallet"`
+	Exchanges float64 `firestore:"exchanges" json:"exchanges"`
 }
 
 type Payment struct {
-	Address       string `bson:"address" json:"address"`
-	Amount        string `bson:"amount" json:"amount"`
-	Coin          string `bson:"coin" json:"coin"`
-	Txid          string `bson:"txid" json:"txid"`
-	Confirmations string `bson:"confirmations" json:"confirmations"`
+	Address       string `firestore:"address" json:"address"`
+	Amount        string `firestore:"amount" json:"amount"`
+	Coin          string `firestore:"coin" json:"coin"`
+	Txid          string `firestore:"txid" json:"txid"`
+	Confirmations string `firestore:"confirmations" json:"confirmations"`
 }
 
 type Properties struct {
-	FeePercentage int  `bson:"fee_percentage" json:"fee_percentage"`
-	Available     bool `bson:"available" json:"available"`
+	FeePercentage int  `firestore:"fee_percentage" json:"fee_percentage"`
+	Available     bool `firestore:"available" json:"available"`
 }
 
 type Config struct {
-	Shift    Properties `bson:"shift" json:"shift"`
-	Deposits Properties `bson:"deposits" json:"deposits"`
-	Vouchers Properties `bson:"vouchers" json:"vouchers"`
-	Orders   Properties `bson:"orders" json:"orders"`
+	Shift    Properties `firestore:"shift" json:"shift"`
+	Deposits Properties `firestore:"deposits" json:"deposits"`
+	Vouchers Properties `firestore:"vouchers" json:"vouchers"`
+	Orders   Properties `firestore:"orders" json:"orders"`
 }
 
 type Order struct {
-	ID                  string                     `bson:"id" json:"id"`
-	UID                 string                     `bson:"uid" json:"uid"`
-	Status              string                     `bson:"status" json:"status"`
-	PaymentInfo         Payment                    `bson:"payment_info" json:"payment_info"`
-	AddressInfo         AddressInformation         `bson:"address_info" json:"address_info"`
-	Delivery            DeliveryOption             `bson:"delivery" json:"delivery"`
-	PersonalizationData PersonalizationInformation `bson:"personalization_data" json:"personalization_data"`
+	ID                  string                     `firestore:"id" json:"id"`
+	UID                 string                     `firestore:"uid" json:"uid"`
+	Status              string                     `firestore:"status" json:"status"`
+	PaymentInfo         Payment                    `firestore:"payment_info" json:"payment_info"`
+	AddressInfo         AddressInformation         `firestore:"address_info" json:"address_info"`
+	Delivery            DeliveryOption             `firestore:"delivery" json:"delivery"`
+	PersonalizationData PersonalizationInformation `firestore:"personalization_data" json:"personalization_data"`
 }
 
 type PersonalizationInformation struct {
-	PersonalData PersonalInformation `bson:"personal_data" json:"personal_data"`
-	AddressData  AddressInformation  `bson:"address_data" json:"address_data"`
+	PersonalData PersonalInformation `firestore:"personal_data" json:"personal_data"`
+	AddressData  AddressInformation  `firestore:"address_data" json:"address_data"`
 }
 
 type PersonalInformation struct {
-	BirthDate   string `bson:"birth_date" json:"birth_date"`
-	CivilState  string `bson:"civil_state" json:"civil_state"`
-	FirstName   string `bson:"first_name" json:"first_name"`
-	LastName    string `bson:"last_name" json:"last_name"`
-	Sex         string `bson:"sex" json:"sex"`
-	HomeNumber  string `bson:"home_number" json:"home_number"`
-	Nationality string `bson:"nationality" json:"nationality"`
-	PassportID  string `bson:"passport_id" json:"passport_id"`
+	BirthDate   string `firestore:"birth_date" json:"birth_date"`
+	CivilState  string `firestore:"civil_state" json:"civil_state"`
+	FirstName   string `firestore:"first_name" json:"first_name"`
+	LastName    string `firestore:"last_name" json:"last_name"`
+	Sex         string `firestore:"sex" json:"sex"`
+	HomeNumber  string `firestore:"home_number" json:"home_number"`
+	Nationality string `firestore:"nationality" json:"nationality"`
+	PassportID  string `firestore:"passport_id" json:"passport_id"`
 }
 
 type DeliveryOption struct {
-	Type            string             `bson:"type" json:"type"`
-	Service         string             `bson:"service" json:"service"`
-	DeliveryAddress AddressInformation `bson:"delivery_address" json:"delivery_address"`
+	Type            string             `firestore:"type" json:"type"`
+	Service         string             `firestore:"service" json:"service"`
+	DeliveryAddress AddressInformation `firestore:"delivery_address" json:"delivery_address"`
 }
 
 type AddressInformation struct {
-	City       string `bson:"city" json:"city"`
-	Country    string `bson:"country" json:"country"`
-	PostalCode string `bson:"postal_code" json:"postal_code"`
-	Email      string `bson:"email" json:"email"`
-	Street     string `bson:"street" json:"street"`
+	City       string `firestore:"city" json:"city"`
+	Country    string `firestore:"country" json:"country"`
+	PostalCode string `firestore:"postal_code" json:"postal_code"`
+	Email      string `firestore:"email" json:"email"`
+	Street     string `firestore:"street" json:"street"`
 }
 
 type Shift struct {
-	ID         string  `bson:"id" json:"id"`
-	UID        string  `bson:"uid" json:"uid"`
-	Status     string  `bson:"status" json:"status"`
-	Timestamp  string  `bson:"timestamp" json:"timestamp"`
-	Payment    Payment `bson:"payment" json:"payment"`
-	Conversion Payment `bson:"conversion" json:"conversion"`
+	ID         string  `firestore:"id" json:"id"`
+	UID        string  `firestore:"uid" json:"uid"`
+	Status     string  `firestore:"status" json:"status"`
+	Timestamp  string  `firestore:"timestamp" json:"timestamp"`
+	Payment    Payment `firestore:"payment" json:"payment"`
+	Conversion Payment `firestore:"conversion" json:"conversion"`
 }
 
 type User struct {
-	ID       string         `bons:"id" json:"id"`
-	Email    string         `bson:"email" json:"email"`
-	KYCData  KYCInformation `bson:"kyc_data" json:"kyc_data"`
-	Role     string         `bson:"role" json:"role"`
-	Shifts   []string       `bson:"shifts" json:"shifts"`
-	Vouchers []string       `bson:"vouchers" json:"vouchers"`
-	Deposits []string       `bson:"deposits" json:"deposits"`
-	Cards    []string       `bson:"cards" json:"cards"`
-	Orders   []string       `bson:"orders" json:"orders"`
+	ID       string         `firestore:"id" json:"id"`
+	Email    string         `firestore:"email" json:"email"`
+	KYCData  KYCInformation `firestore:"kyc_data" json:"kyc_data"`
+	Role     string         `firestore:"role" json:"role"`
+	Shifts   []string       `firestore:"shifts" json:"shifts"`
+	Vouchers []string       `firestore:"vouchers" json:"vouchers"`
+	Deposits []string       `firestore:"deposits" json:"deposits"`
+	Cards    []string       `firestore:"cards" json:"cards"`
+	Orders   []string       `firestore:"orders" json:"orders"`
 }
 
 type KYCInformation struct{}
 
 type Voucher struct {
-	ID                string  `bson:"id" json:"id"`
-	UID               string  `bson:"uid" json:"uid"`
-	VoucherID         string  `bson:"voucher_id" json:"voucher_id"`
-	VariantID         string  `bson:"variant_id" json:"variant_id"`
-	FiatAmount        string  `bson:"fiat_amount" json:"fiat_amount"`
-	Name              string  `bson:"name" json:"name"`
-	PaymentData       Payment `bson:"payment_data" json:"payment_data"`
-	BitcouPaymentData Payment `bson:"bitcou_payment_data" json:"bitcou_payment_data"`
-	RedeemCode        string  `bson:"redeem_code" json:"redeem_code"`
-	Status            string  `bson:"status" json:"status"`
-	Timestamp         string  `bson:"timestamp" json:"timestamp"`
+	ID                string  `firestore:"id" json:"id"`
+	UID               string  `firestore:"uid" json:"uid"`
+	VoucherID         string  `firestore:"voucher_id" json:"voucher_id"`
+	VariantID         string  `firestore:"variant_id" json:"variant_id"`
+	FiatAmount        string  `firestore:"fiat_amount" json:"fiat_amount"`
+	Name              string  `firestore:"name" json:"name"`
+	PaymentData       Payment `firestore:"payment_data" json:"payment_data"`
+	BitcouPaymentData Payment `firestore:"bitcou_payment_data" json:"bitcou_payment_data"`
+	RedeemCode        string  `firestore:"redeem_code" json:"redeem_code"`
+	Status            string  `firestore:"status" json:"status"`
+	Timestamp         string  `firestore:"timestamp" json:"timestamp"`
 }
 
 type Deposit struct {
-	ID           string  `bson:"id" json:"id"`
-	UID          string  `bson:"uid" json:"uid"`
-	Payment      Payment `bson:"payment" json:"payment"`
-	AmountInPeso string  `bson:"amount_in_peso" json:"amount_in_peso"`
-	CardCode     string  `bson:"card_code" json:"card_code"`
-	Status       string  `bson:"status" json:"status"`
-	Timestamp    string  `bson:"timestamp" json:"timestamp"`
+	ID           string  `firestore:"id" json:"id"`
+	UID          string  `firestore:"uid" json:"uid"`
+	Payment      Payment `firestore:"payment" json:"payment"`
+	AmountInPeso string  `firestore:"amount_in_peso" json:"amount_in_peso"`
+	CardCode     string  `firestore:"card_code" json:"card_code"`
+	Status       string  `firestore:"status" json:"status"`
+	Timestamp    string  `firestore:"timestamp" json:"timestamp"`
 }
 
 type BodyReq struct {
