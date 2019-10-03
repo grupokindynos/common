1402064568b1056cfe91905fef42db3217146a20
PrettyBoyHelios <helios@polispay.org>
2019-10-03 13:20:23 -0500

    Revert "Model Update"
    This reverts commit 846eec2df15cb3f97ce1cf42b01bac0308f00394.

--- plutus/models.go
+++ plutus/models.go
@@ -1,29 +1,8 @@
 package plutus
 
-//Plutus is the model for the response from the Plutus microservice
-type Plutus struct {
-	Data   interface{} `json:"data"`
-	Status int64       `json:"status"`
-}
-
-type PlutusEncoded struct {
-	Data   string `json:"data"`
-	Status int64  `json:"status"`
-}
-
-//PlutusAddress is the model for the response from the Plutus address microservice
-type PlutusAddress struct {
-	Data string `json:"address"`
-}
-
 //Address is the model for the GET plutus address validation method
-type ValidAddress struct {
-	Valid bool `json:"valid"`
-}
-
-//Address is the model for the response from the hot-wallets address
 type Address struct {
-	Address string `json:"address"`
+	Valid bool `json:"valid"`
 }
 
 //Status is the model for the GET plutus status method
@@ -46,13 +25,7 @@ type Info struct {
 	Connections int    `json:"connections"`
 }
 
-//PlutusBalance is the model for the response from the Plutus address microservice
-type PlutusBalance struct {
-	Data   Balance "json:data"
-	Status int64   "json:status"
-}
-
-//Balance is the model for the hot-wallets balance
+//Balance is the model for the GET plutus balance method
 type Balance struct {
 	Confirmed   float64 `json:"confirmed"`
 	Unconfirmed float64 `json:"unconfirmed"`
