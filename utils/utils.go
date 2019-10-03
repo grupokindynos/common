f19b4806053bae91e548bf318496d5f98837b201
Cronos <eabz@polispay.org>
2019-10-03 11:59:08 -0500

    Random String generator

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,14 @@
 package utils
 
+import (
+	"math/rand"
+	"time"
+)
+
+var seededRand *rand.Rand = rand.New(
+	rand.NewSource(time.Now().UnixNano()))
+
+
 func Contains(a []string, x string) bool {
 	for _, n := range a {
 		if x == n {
@@ -8,3 +17,14 @@ func Contains(a []string, x string) bool {
 	}
 	return false
 }
+
+func RandomString() string {
+	charset := "abcdefghijklmnopqrstuvwxyz" +
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, 50)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
