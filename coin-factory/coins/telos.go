dae4bcaab4755200274cd8b514d0f0be9d690aa1
Cronos <eabz@polispay.org>
2019-10-04 11:29:34 -0500

    New coin

---
+++ coin-factory/coins/telos.go
@@ -0,0 +1,26 @@
+package coins
+
+var Telos = Coin{
+	Tag:  "TELOS",
+	Name: "teloscoin",
+	Rates: RatesSource{
+		Exchange:         "graviex",
+		FallBackExchange: "southxchange",
+	},
+	BlockchainInfo: BlockchainInfo{
+		ExternalSource:   "telos.polispay.org",
+		BlockTime:        1,
+		MinConfirmations: 6,
+	},
+	RpcMethods: RPCMethods{
+		GetWalletInfo:              "getwalletinfo",
+		GetBlockchainInfo:          "getblockchaininfo",
+		GetNetworkInfo:             "getnetworkinfo",
+		GetNewAddress:              "getnewaddress",
+		SendToAddress:              "sendtoaddress",
+		ValidateAddress:            "validateaddress",
+		GetRawTransaction:          "getrawtransaction",
+		DecodeRawTransaction:       "decoderawtransaction",
+		GetRawTransactionVerbosity: true,
+	},
+}
