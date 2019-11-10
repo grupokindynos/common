807d458ea5f3b4f596ba063041f934385a88b440
Enrique Berrueta <eabz@polispay.org>
2019-11-10 11:01:13 -0600

    Reorg

--- utils/utils.go
+++ utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
@@ -25,4 +27,16 @@ func RandomString() string {
 		b[i] = charset[seededRand.Intn(len(charset))]
 	}
 	return string(b)
+}
+
+func UnitToSat(u float64) int64 {
+	return int64(u / math.Pow10(0))
+}
+
+func SatToUnit(s int64) float64 {
+	return float64(s) / math.Pow10(8)
+}
+
+func UnitToString(u float64) string {
+	return fmt.Sprintf("%f", u)
 }
