340ead09e2ab966e06e09809e824cc919e6e444e
Enrique <eabz@polispay.org>
2019-12-03 11:40:58 -0600

    Fix iota

--- hestia/ladon_models.go
+++ hestia/ladon_models.go
@@ -11,8 +11,8 @@ const (
 	VoucherStatusComplete
 	VoucherStatusRefundTotal
 	VoucherStatusRefundFee
-	VoucherStatusRefundedPartially
 	VoucherStatusRefunded
+	VoucherStatusRefundedPartially
 )
 
 var (
