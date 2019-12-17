a0f314bcc6368b8a87920d55998e0f9900ecd7c5
Enrique <eabz@polispay.org>
2019-12-16 20:02:49 -0600

    Reduce min conf

--- coin-factory/coins/digibyte.go
+++ coin-factory/coins/digibyte.go
@@ -12,7 +12,7 @@ var Digibyte = Coin{
 	BlockchainInfo: BlockchainInfo{
 		ExternalSource:   "dgb2.trezor.io",
 		BlockTime:        0.25,
-		MinConfirmations: 25,
+		MinConfirmations: 10,
 	},
 	NetParams: &chaincfg.Params{
 		Bech32HRPSegwit: "dgb",
