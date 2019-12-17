5b6e2752ce27ce5b9b07b6608f93d6873a8a213f
PrettyBoyHelios <helios@polispay.org>
2019-12-17 12:21:41 -0600

    handling of voucher error

--- responses/responses.go
+++ responses/responses.go
@@ -3,13 +3,19 @@ package responses
 import (
 	"errors"
 	"github.com/gin-gonic/gin"
+	commonErrors "github.com/grupokindynos/common/errors"
 )
 
 // GlobalResponseError is used to wrap all the errored API responses under the same model.
 // Automatically detect if there is an error and return status and code according
 func GlobalResponseError(result interface{}, err error, c *gin.Context) *gin.Context {
 	if err != nil {
-		c.JSON(500, gin.H{"message": "Error", "error": err.Error(), "status": -1})
+		switch err {
+		case commonErrors.ErrorVoucherLimit:
+			c.JSON(407, gin.H{"message": "voucher daily limit exceeded", "error": err.Error(), "status":1})
+		default:
+			c.JSON(500, gin.H{"message": "Error", "error": err.Error(), "status": -1})
+		}
 	} else {
 		c.JSON(200, gin.H{"data": result, "status": 1})
 	}
