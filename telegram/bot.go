efe8a60b55724b83cb96a6fafb82a622a226099e
Aether-eum <60359292+Aether-eum@users.noreply.github.com>
2020-03-04 11:51:56 -0600

    cleanup

--- telegram/bot.go
+++ telegram/bot.go
@@ -21,7 +21,7 @@ func NewTelegramBot(apiKey string, chatId string) TelegramBot {
 		_ = logger.SingleLog("telegram_log", "telegram", "NewTelegramBot - "+err.Error())
 		return TelegramBot{isWorking: false}
 	}
-	//bot.Debug = true
+
 	tb := TelegramBot{
 		telegramBot: *bot,
 		isWorking:   true,
@@ -42,7 +42,6 @@ func (tb *TelegramBot) SendMessage(message string) {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 	chatId, _ := strconv.ParseInt(tb.chatID, 10, 64)
-	fmt.Println(chatId)
 	_, _ = tb.telegramBot.Send(tgbotapi.NewMessage(chatId, message))
 }
 
