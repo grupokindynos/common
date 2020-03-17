474094a0f5d03f2b7cfef7b8aec1a497fe5512d4
helios <helios@polispay.org>
2020-03-17 13:47:49 -0600

    Update responses.go

--- responses/responses.go
+++ responses/responses.go
@@ -34,7 +34,7 @@ func GlobalResponseNoAuth(c *gin.Context) *gin.Context {
 }
 
 // GlobalResponseMRT is used to wrap all responses using a MRT token
-func GlobalResponseMRT(header string, body string, c *gin.Context) *gin.Context {
+func GlobalResponseMRT(body string, c *gin.Context) *gin.Context {
 	c.Header("service", header)
 	c.JSON(200, body)
 	return c
