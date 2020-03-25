49a2c9f8a4996ce275d172d2956df633dbc56673
helios <helios@polispay.org>
2020-03-25 10:20:44 -0600

    New ShiftV2 Statuses

--- hestia/tyche_models.go
+++ hestia/tyche_models.go
@@ -24,6 +24,37 @@ var (
 	}
 )
 
+func GetShiftStatusV2String(status ShiftStatus) string {
+	value, ok := ShiftStatusStr[status]
+	if !ok {
+		return ""
+	}
+	return value
+}
+
+type ShiftStatusv2 int
+
+const (
+	ShiftStatusV2Pending ShiftStatusv2 = iota
+	ShiftStatusV2Confirming
+	ShiftStatusV2Confirmed
+	ShiftStatusV2Error
+	ShiftStatusV2Refund
+	ShiftStatusV2Refunded
+	ShiftStatusV2Complete
+)
+
+var (
+	ShiftStatusV2Str = map[ShiftStatus]string{
+		0: "CREATED",
+		1: "CONFIRMED",
+		2: "PROCESSING_ORDERS",
+		3: "PARTIALLY_COMPLETED", // One of the two orders completed succesfully
+		4: "COMPLETED",           //
+		5: "SENT_TO_USER",
+	}
+)
+
 func GetShiftStatusString(status ShiftStatus) string {
 	value, ok := ShiftStatusStr[status]
 	if !ok {
