5193a042b08ecbbe87c120e6df737cd5d296b7e1
oedipusK <58442178+oedipusK@users.noreply.github.com>
2020-06-18 13:02:24 -0500

    Fixed data types

--- herodotus/herodotus.go
+++ herodotus/herodotus.go
@@ -3,11 +3,11 @@ package herodotus
 import "github.com/grupokindynos/common/hestia"
 
 type VoucherV2Filters struct {
-	Status map[hestia.VoucherStatusV2]bool `json:"status"`
+	Status []hestia.VoucherStatusV2 `json:"status"`
 	FromTimestamp int64 `json:"from_timestamp"`
 	ToTimestamp int64 `json:"to_timestamp"`
-	UserId map[string]bool `json:"user_id"`
-	VoucherId map[int]bool `json:"voucher_id"`
-	ProviderId map[string]bool `json:"provider_id"`
-	Coin map[string]bool `json:"coin"`
+	UserId []string `json:"user_id"`
+	VoucherId []int `json:"voucher_id"`
+	ProviderId []string `json:"provider_id"`
+	Coin []string `json:"coin"`
 }
