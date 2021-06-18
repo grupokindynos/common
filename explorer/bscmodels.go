33308020fe5011776f34dd037450ff47b82f9cb5
Luis Correa <helios@polispay.org>
2021-06-18 09:57:36 -0500

    Added BSC API Models

--- explorer/bscmodels.go
+++ explorer/bscmodels.go
@@ -1,5 +1,9 @@
 package explorer
 
+
+/*
+	BSC Scan Models
+ */
 type BSCResponse struct {
 	JsonRPC string `json:"jsonrpc"`
 	ID      int    `json:"id"`
@@ -10,6 +14,9 @@ type BSCResponse struct {
 	Result  string `json:"result"`
 }
 
+/*
+	BSC API Models hosted at https://pp-bsc-api.herokuapp.com/
+ */
 type BSCBase struct {
 	Status int32 `json:"status"`
 	Error string `json:"error"`
@@ -26,4 +33,45 @@ type BSCTxInfoResponse struct {
 			ReceivedAmount float64 `json:"received_amount"`
 		}`json:"tx_info"`
 	} `json:"data"`
+}
+
+type BSCAddressResponse struct {
+	BSCBase
+	Data struct{
+		Address string `json:"address"`
+	} `json:"data"`
+}
+
+type BSCBalanceResponse struct {
+	BSCBase
+	Data struct{
+		Balance float64 `json:"balance"`
+	} `json:"data"`
+}
+
+type BSCSwapInfo struct {
+	CoinFrom string `json:"coin_from"`
+	AmountIn int64 `json:"amount_in"`
+	AmountOut int64 `json:"amount_out"`
+}
+
+type BSCSwapResponse struct {
+	BSCBase
+	Data struct{
+		Path []string `json:"path"`
+		TxID string `json:"tx_id"`
+	} `json:"data"`
+}
+
+type BSCWithdrawResponse struct {
+	BSCBase
+	Data struct{
+		TxID string `json:"tx_id"`
+	} `json:"data"`
+}
+
+type BSCWithdrawInput struct {
+	Address string `json:"address"`
+	Asset string `json:"asset"`
+	Amount float64 `json:"amount"`
 }
